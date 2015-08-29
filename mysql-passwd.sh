d8c0752ba09aa4d917a4052ec89ca239c216baec
Oleg Morozenkov <omorozenkov@gmail.com>
2015-08-29 20:36:28 +0300

    Update mysql-passwd.sh

--- mysql-passwd.sh
+++ mysql-passwd.sh
@@ -8,14 +8,17 @@ fi
 
 mysqld --skip-grant-tables --skip-networking &
 
+echo "Waiting mariadb to start."
 for i in {30..0}; do
 	if echo 'SELECT 1' | mysql -u root &> /dev/null; then
 		break
 	fi
+	echo -n "."
 	sleep 1
 done
+echo
 if [ "$i" = 0 ]; then
-	echo >&2 "MySQL init process failed."
+	echo >&2 "mariadb failed to start."
 	exit 1
 fi
 
