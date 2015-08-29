b4643e6366d872b44ec32ed8f6655d5db6083254
Oleg Morozenkov <omorozenkov@gmail.com>
2015-08-29 20:25:07 +0300

    Update mysql-passwd.sh

--- mysql-passwd.sh
+++ mysql-passwd.sh
@@ -7,6 +7,18 @@ then
 fi
 
 mysqld --skip-grant-tables --skip-networking &
+
+for i in {30..0}; do
+	if echo 'SELECT 1' | mysql -u root &> /dev/null; then
+		break
+	fi
+	sleep 1
+done
+if [ "$i" = 0 ]; then
+	echo >&2 "MySQL init process failed."
+	exit 1
+fi
+
 mysql -u root <<-END
 	FLUSH PRIVILEGES;
 	SET PASSWORD FOR '${1}'@'localhost' = PASSWORD('${2}');
