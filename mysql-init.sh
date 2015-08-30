0411396822fcd58d3c78ff77722ea74319cca8be
Oleg Morozenkov <omorozenkov@gmail.com>
2015-08-30 18:44:24 +0300

    Update mysql-init.sh

--- mysql-init.sh
+++ mysql-init.sh
@@ -2,20 +2,21 @@
 
 set -e
 
-DATADIR=/var/lib/mysql
-
 if [ ! -d "$DATADIR/mysql" ]
 then
-	mkdir -p "$DATADIR"
-	chown -R mysql:mysql "$DATADIR"
+	mkdir -p "/var/lib/mysql"
+	chown -R mysql:mysql "/var/lib/mysql"
 	echo "Initializing mysql."
-	mysql_install_db --user=mysql --datadir="$DATADIR"
+	mysql_install_db --user=mysql --datadir="/var/lib/mysql"
 	echo "Done."
 
-	for f in /mysql-init.d/*
-	do
-		echo "Running mysql init script $f"
-		sh "$f"
-	done
+	if [ -d "/mysql-init.d" ]
+	then
+		for f in /mysql-init.d/*
+		do
+			echo "Running mysql init script $f"
+			sh "$f"
+		done
+	fi
 fi
 
